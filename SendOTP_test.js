e771144839f50a35ec0a2ef85cd78a64c1b39f0b
teepakorn Kumvong <teeprakorn.toey@gmail.com>
2024-08-07 14:30:13 +0700

    VERSION 0.3.5

--- SendOTP_test.js
+++
@@ -1,6 +0,0 @@
-// app.js
-const sendOTPEmail = require('./OTP_Email/SendEmail');
-
-const recipientEmail = 'teeprakorn.toey@gmail.com'; // replace with the recipient's email address
-
-sendOTPEmail(recipientEmail);
